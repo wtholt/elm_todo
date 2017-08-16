1843157bd4c1f8868a6bad94d84e26dec3274171
William Holt <holtw@musc.edu>
2017-08-16 15:00:23 -0400

    added foreman

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,6 +3,7 @@ class TodosController < ApplicationController
   def index
     @todos = Todo.all
     respond_to do |format|
+      format.html
       format.json { render json: @todos }
     end
   end
