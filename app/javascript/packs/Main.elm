7245b349261c02a7ddff79d1bcb2f684ecaa1d2d
William Holt <holtw@musc.edu>
2017-08-16 16:40:07 -0400

    updated and playing around with elm

--- app/javascript/packs/Main.elm
+++ app/javascript/packs/Main.elm
@@ -1,44 +1,95 @@
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Decode
+
 
 
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+  Html.program
+    { init = init "dogs"
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
 
 
 -- MODEL
 
-type alias Model = Int
 
-model : Model
-model =
-  0
+type alias Model =
+  { topic : String
+  , gifUrl : String
+  }
+
+
+init : String -> (Model, Cmd Msg)
+init topic =
+  ( Model topic "waiting.gif"
+  , getRandomGif topic
+  )
+
 
 
 -- UPDATE
 
-type Msg = Increment | Decrement | Reset
 
-update : Msg -> Model -> Model
+type Msg
+  = MorePlease
+  | NewGif (Result Http.Error String)
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Increment ->
-      model + 1
+    MorePlease ->
+      (model, getRandomGif model.topic)
 
-    Decrement ->
-      model - 1
+    NewGif (Ok newUrl) ->
+      (Model model.topic newUrl, Cmd.none)
 
-    Reset ->
-      model - model
+    NewGif (Err _) ->
+      (model, Cmd.none)
+
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
   div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    , button [ onClick Reset ] [ text "Reset" ]
+    [ h2 [] [text model.topic]
+    , button [ onClick MorePlease ] [ text "More Please!" ]
+    , br [] []
+    , img [src model.gifUrl] []
     ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- HTTP
+
+
+getRandomGif : String -> Cmd Msg
+getRandomGif topic =
+  let
+    url =
+      "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+  in
+    Http.send NewGif (Http.get url decodeGifUrl)
+
+
+decodeGifUrl : Decode.Decoder String
+decodeGifUrl =
+  Decode.at ["data", "image_url"] Decode.string
