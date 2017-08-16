302634f6a55a7b7bc86a2f15778c482c859ec824
William Holt <holtw@musc.edu>
2017-08-16 14:29:36 -0400

    stuff

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,32 @@
 class TodosController < ApplicationController
 
   def index
+    @todos = Todo.all
+    respond_to do |format|
+      format.json { render json: @todos }
+    end
+  end
 
+  def create
+    @todo = Todo.create(todo_params)
+    respond_to do |format|
+      format.json
+    end
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update_attributes(todo_params)
+    respond_to do |format|
+      format.json
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    respond_to do |format|
+      format.json
+    end
   end
 end
