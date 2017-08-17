24b753e4a2c3fcbed1f36d957c1d3c7a2d679dc6
William Holt <holtw@musc.edu>
2017-08-17 15:41:02 -0400

    more elm experimentation

--- app/javascript/packs/Main.elm
+++ app/javascript/packs/Main.elm
@@ -1,61 +1,97 @@
-module Main exposing (..)
+-- Read all about this program in the official Elm guide:
+-- https://guide.elm-lang.org/architecture/user_input/forms.html
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+main =
+  Html.beginnerProgram
+    { model = model
+    , view = view
+    , update = update
+    }
+
+
 
 -- MODEL
 
+
 type alias Model =
-  {
+  { name : String
+  , password : String
+  , passwordAgain : String
+  , age : Maybe Int
   }
 
--- INIT
 
-init : (Model, Cmd Message)
-init =
-  (Model, Cmd.none)
+model : Model
+model =
+  Model "" "" "" ""
 
--- VIEW
 
-view : Model -> Html Message
-view model =
-  -- The inline style is being used for example purposes in order to keep this example simple and
-  -- avoid loading additional resources. Use a proper stylesheet when building your own app.
-
-  div [ class "form-group" ]
-  [ label [ class "col-sm-2 control-label" ]
-    [ text "Task" ]
-  , div [ class "col-sm-10" ]
-    [ input [ class "form-control", placeholder "Task" ]
-      []
-    ]
-  ]
-
--- MESSAGE
-
-type Message
-  = None
 
 -- UPDATE
 
-update : Message -> Model -> (Model, Cmd Message)
-update message model =
-  (model, Cmd.none)
 
--- SUBSCRIPTIONS
+type Msg
+    = Name String
+    | Password String
+    | PasswordAgain String
+    | Age Maybe Int
 
-subscriptions : Model -> Sub Message
-subscriptions model =
-  Sub.none
 
--- MAIN
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Name name ->
+      { model | name = name }
 
-main : Program Never Model Message
-main =
-  Html.program
-    {
-      init = init,
-      view = view,
-      update = update,
-      subscriptions = subscriptions
-    }
+    Password password ->
+      { model | password = password }
+
+    PasswordAgain password ->
+      { model | passwordAgain = password }
+
+    Age age ->
+      { model | age = String.toInt(age) }
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ type_ "text", placeholder "Name", onInput Name ] []
+    , input [ type_ "password", placeholder "Password", onInput Password ] []
+    , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+    , input [ type_ "age", placeholder "Age", onInput Age ] []
+    , viewValidation model
+    , passwordLength model
+    ]
+
+
+viewValidation : Model -> Html msg
+viewValidation model =
+  let
+    (color, message) =
+      if model.password == model.passwordAgain then
+        ("green", "OK")
+      else
+        ("red", "Passwords do not match!")
+  in
+    div [ style [("color", color)] ] [ text message ]
+
+passwordLength : Model -> Html msg
+passwordLength model =
+  let
+    (display, message) =
+      if String.length model.password >= 8 then
+        ("hide", "")
+      else
+        ("", "Password must be at least 8 characters")
+  in
+    div [ class display ] [ text message ]
+
